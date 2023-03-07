031a4d396cee2b3a0748a045d1ae285df52bc35f
Luis Fernando <luis.silva9902@gmail.com>
2023-03-07 20:40:42 -0300

    Created modules and first server in Node

---
+++ first-steps-in-node/modules/index.js
@@ -0,0 +1,7 @@
+const { printName, lastName } = require('./printName')
+
+printName(`Luis Fernando ${lastName}`)
+
+const os = require('os')
+
+console.log(os.type())
---
+++ first-steps-in-node/modules/printName.js
@@ -0,0 +1,10 @@
+exports.printName = (name) => {
+  console.log(name)
+}
+
+exports.lastName = 'Da Silva'
+
+// module.exports = {
+//   printName,
+//   lastName
+// }
