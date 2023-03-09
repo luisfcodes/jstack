40d264b7dca3effec9ca5a0c62d4e7ab626659f9
Luis Fernando <luis.silva9902@gmail.com>
2023-03-08 21:43:29 -0300

    Created mocks, controller and routes

--- first-steps-in-node/firstapi/src/index.js
+++ first-steps-in-node/firstapi/src/index.js
@@ -1,8 +1,27 @@
 const http = require('http')
 
+const routes = require('./routes')
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/html' })
-  response.end('<h1>Hello World!</h1>')
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === request.url && routeObj.method === request.method
+  ))
+
+  if(route){
+    route.handler(request, response)
+  } else {
+    response.writeHead(404, { 'Content-type': 'text/html' })
+    response.end(`Cannot ${request.method} ${request.url}`)
+  }
+
+  // if(request.method === 'GET' && request.url === '/users'){
+  //   UserController.listUsers(request, response)
+  // } else {
+  //   response.writeHead(404, { 'Content-type': 'text/html' })
+  //   response.end(`Cannot ${request.method} ${request.url}`)
+  // }
 })
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
---
+++ first-steps-in-node/firstapi/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Luis Fernando'
+  },
+  {
+    id: 2,
+    name: 'Amanda Soares'
+  },
+  {
+    id: 3,
+    name: 'Rosi Soares'
+  },
+]
