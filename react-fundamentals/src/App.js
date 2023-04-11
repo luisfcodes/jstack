094fa51f90cb688a0d90b6850ed8ebfb7a681d7f
Luis Fernando <luis.silva9902@gmail.com>
2023-04-11 07:25:42 -0300

    Working with State

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -1,25 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./Header"
 import Post from "./Post"
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-  { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-  { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-]
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+  ])
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState, 
+      {
+      id: Math.random(),
+      title: `Title#0${prevState.length + 1}`,
+      subtitle: `Sub#0${prevState.length + 1}`,
+      likes: 50
+      },
+    ])
+  }
+
   return (
     <>
       <Header title="JStack's Blog">
         <h2>Posts da semana</h2>
+        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
 
       <hr />
 
       {posts.map(post => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           title={post.title}
           subtitle={post.subtitle}
